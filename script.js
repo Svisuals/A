868b6f120e28f9f1c909a37eb5786a7216093481
Svisuals <131906597+Svisuals@users.noreply.github.com>
2023-05-06 16:13:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const container = document.querySelector('.container');
-const iframeContainer = document.querySelectorAll('.iframe-container');
-const arrowLeft = document.getElementById('arrow-left');
-const arrowRight = document.getElementById('arrow-right');
-
+// Get all iframe containers and set the initial index to 0
+const iframeContainers = document.querySelectorAll('.iframe-container');
 let currentIndex = 0;
 
+// Show the current iframe and hide the rest
+const showIframe = () => {
+  iframeContainers.forEach((container, index) => {
+    if (index === currentIndex) {
+      container.classList.add('active');
+    } else {
+      container.classList.remove('active');
+    }
+  });
+};
+
+// Add event listener to the buttons to change the index and show the iframe
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    currentIndex = index;
+    showIframe();
+  });
+});
+
+// Show the initial iframe
+showIframe();
