3fd30284a3c067930c012b1610f9c05d23c06251
Svisuals <131906597+Svisuals@users.noreply.github.com>
2023-05-06 16:39:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const sitios = document.querySelectorAll(".sitio");
-const botones = document.querySelectorAll(".boton");
+let site1 = document.getElementById("site1");
+let site2 = document.getElementById("site2");
 
-let sitioActivo = 1;
-
-function cambiarSitio(numSitio) {
-    sitios.forEach(sitio => sitio.classList.remove("activo"));
-    botones.forEach(boton => boton.classList.remove("activo"));
-    sitioActivo = numSitio;
-    sitios[sitioActivo - 1].classList.add("activo");
-    botones[sitioActivo - 1].classList.add("activo");
+function showSite(siteNum) {
+    if (siteNum == 1) {
+        site1.classList.add("active");
+        site2.classList.remove("active");
+        document.querySelector(".buttons button:nth-child(1)").classList.add("active");
+        document.querySelector(".buttons button:nth-child(2)").classList.remove("active");
+    } else {
+        site2.classList.add("active");
+        site1.classList.remove("active");
+        document.querySelector(".buttons button:nth-child(2)").classList.add("active");
+        document.querySelector(".buttons button:nth-child(1)").classList.remove("active");
+    }
 }
 
-botones.forEach((boton, index) => {
-    boton.textContent = index + 1;
-    boton.addEventListener("click", () => {
-        cambiarSitio(index + 1);
-    });
+document.querySelector(".buttons button:nth-child(1)").addEventListener("click", function() {
+    showSite(1);
 });
 
-cambiarSitio(sitioActivo);
+document.querySelector(".buttons button:nth-child(2)").addEventListener("click", function() {
+    showSite(2);
+});
+
 
