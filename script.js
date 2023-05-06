11b9e1d6e0debc653538d5f1120a4abab136711d
Svisuals <131906597+Svisuals@users.noreply.github.com>
2023-05-06 16:35:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-let site1 = document.getElementById("site1");
-let site2 = document.getElementById("site2");
+const sitios = document.querySelectorAll(".sitio");
+const botones = document.querySelectorAll(".boton");
 
-function showSite(siteNum) {
-    if (siteNum == 1) {
-        site1.classList.add("active");
-        site2.classList.remove("active");
-        document.querySelector(".buttons button:nth-child(1)").classList.add("active");
-        document.querySelector(".buttons button:nth-child(2)").classList.remove("active");
-    } else {
-        site2.classList.add("active");
-        site1.classList.remove("active");
-        document.querySelector(".buttons button:nth-child(2)").classList.add("active");
-        document.querySelector(".buttons button:nth-child(1)").classList.remove("active");
-    }
+let sitioActivo = 1;
+
+function cambiarSitio(numSitio) {
+    sitios.forEach(sitio => sitio.classList.remove("activo"));
+    botones.forEach(boton => boton.classList.remove("activo"));
+    sitioActivo = numSitio;
+    sitios[sitioActivo - 1].classList.add("activo");
+    botones[sitioActivo - 1].classList.add("activo");
 }
 
-document.querySelector(".buttons button:nth-child(1)").addEventListener("click", function() {
-    showSite(1);
+botones.forEach((boton, index) => {
+    boton.textContent = index + 1;
+    boton.addEventListener("click", () => {
+        cambiarSitio(index + 1);
+    });
 });
 
-document.querySelector(".buttons button:nth-child(2)").addEventListener("click", function() {
-    showSite(2);
-});
+cambiarSitio(sitioActivo);
 
