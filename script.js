acb624678a82a6477931630a84e4dd780ac50942
Svisuals <131906597+Svisuals@users.noreply.github.com>
2023-05-06 16:06:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container');
+const iframeContainer = document.querySelectorAll('.iframe-container');
+const arrowLeft = document.getElementById('arrow-left');
+const arrowRight = document.getElementById('arrow-right');
+
+let currentIndex = 0;
+
